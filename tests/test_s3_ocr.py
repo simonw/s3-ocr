e686eb282a8d2a7250a1b9c5414173241d4b6339
Simon Willison <swillison@gmail.com>
2022-06-29 15:36:24 -0700

    test_status, refs #4

--- tests/test_s3_ocr.py
+++ tests/test_s3_ocr.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 from s3_ocr.cli import cli
 import json
+import pytest
 
 
 def test_start_creates_s3_ocr_json(s3, textract):
@@ -13,3 +14,34 @@ def test_start_creates_s3_ocr_json(s3, textract):
     content = s3.get_object(Bucket="my-bucket", Key="blah.pdf.s3-ocr.json")
     decoded = json.loads(content["Body"].read())
     assert set(decoded.keys()) == {"job_id", "etag"}
+
+
+@pytest.mark.parametrize(
+    "files,expected",
+    (
+        ([], "0 complete out of 0 jobs\n"),
+        (
+            [
+                ("blah.pdf", b""),
+                ("blah.pdf.s3-ocr.json", b'{"job_id": "x", "etag": x"}'),
+            ],
+            "0 complete out of 1 jobs\n",
+        ),
+        (
+            [
+                ("blah.pdf", b""),
+                ("blah.pdf.s3-ocr.json", b'{"job_id": "x", "etag": x"}'),
+                ("textract-output/x/1", b"{}"),
+            ],
+            "1 complete out of 1 jobs\n",
+        ),
+    ),
+)
+def test_status(s3, files, expected):
+    runner = CliRunner()
+    for name, content in files:
+        s3.put_object(Bucket="my-bucket", Key=name, Body=content)
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["status", "my-bucket"])
+        assert result.exit_code == 0
+        assert result.output == expected
