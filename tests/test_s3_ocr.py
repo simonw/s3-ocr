b81b6ac621782511a8658cce0ff67eb5c2e9d8bf
Simon Willison <swillison@gmail.com>
2022-06-29 15:31:17 -0700

    test_start_creates_s3_ocr_json using moto, refs #4

--- tests/test_s3_ocr.py
+++ tests/test_s3_ocr.py
@@ -1,10 +1,15 @@
 from click.testing import CliRunner
 from s3_ocr.cli import cli
+import json
 
 
-def test_version():
+def test_start_creates_s3_ocr_json(s3, textract):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["--version"])
+        result = runner.invoke(cli, ["start", "my-bucket"])
         assert result.exit_code == 0
-        assert result.output.startswith("cli, version ")
+    bucket_contents = s3.list_objects_v2(Bucket="my-bucket")["Contents"]
+    assert {b["Key"] for b in bucket_contents} == {"blah.pdf", "blah.pdf.s3-ocr.json"}
+    content = s3.get_object(Bucket="my-bucket", Key="blah.pdf.s3-ocr.json")
+    decoded = json.loads(content["Body"].read())
+    assert set(decoded.keys()) == {"job_id", "etag"}
